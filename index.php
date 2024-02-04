e72361a425e5e7da56044bf8bfe48657bfe8dc76
Tiago França <tiago@tiagofranca.com>
2024-02-04 16:27:59 -0300

    adjust on include
    On branch v2.x
    	modified:   index.php

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <?php
 
-if (!defined('APP_VERSION'))
-{
-	define('APP_VERSION', '0.0.0');
-	define('APP_INDEX_ROOT_FILE', __FILE__);
-	define('APP_INDEX_ROOT_PATH', str_replace('\\', '/', rtrim(dirname(__FILE__), '\\/').'/'));
+if (!defined('APP_VERSION')) {
+    define('APP_VERSION', '0.0.0');
+    define('APP_INDEX_ROOT_FILE', __FILE__);
+    define('APP_INDEX_ROOT_PATH', str_replace('\\', '/', rtrim(dirname(__FILE__), '\\/') . '/'));
 }
 
-if (file_exists(APP_INDEX_ROOT_PATH.'rainloop/v/'.APP_VERSION.'/include.php'))
-{
-	include APP_INDEX_ROOT_PATH.'rainloop/v/'.APP_VERSION.'/include.php';
+if (file_exists(APP_INDEX_ROOT_PATH . 'rainloop/v/' . APP_VERSION . '/include.php')) {
+    echo '[105] Missing version directory';
+    exit(105);
 }
-else
-{
-	echo '[105] Missing version directory';
-	exit(105);
-}
+
+require APP_INDEX_ROOT_PATH . 'rainloop/v/' . APP_VERSION . '/include.php';
